984eb8eb251cbb9927f9301dd868602e49f4596b
Adrian Sian <adriansian@gmail.com>
2021-11-17 11:29:55 +0800

    starlte: arcadia

--- bliss_starlte.mk
+++ bliss_starlte.mk
@@ -24,22 +24,19 @@ $(call inherit-product, device/samsung/starlte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_INCLUDE_PIXEL_CHARGER := true
-TARGET_GAPPS_ARCH := arm64
-FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+#Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := starlte
-PRODUCT_NAME := aosp_starlte
+PRODUCT_NAME := bliss_starlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G960F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-TARGET_BOOT_ANIMATION_RES := 1440
 
 BUILD_FINGERPRINT := "samsung/starltexx/starlte:10/QP1A.190711.020/G960FXXUCFTK1:user/release-keys"
 
